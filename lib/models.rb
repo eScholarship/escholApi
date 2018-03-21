acb587c5220b726274d72ef45da6785aeba32987
Martin Haye <martin.haye@ucop.edu>
2018-03-21 15:09:10 -0700

    Added item authors to API.

--- lib/models.rb
+++ lib/models.rb
@@ -1,6 +1,9 @@
 class Item < Sequel::Model
   unrestrict_primary_key
 end
+class ItemAuthor < Sequel::Model
+  unrestrict_primary_key
+end
 class Unit < Sequel::Model
   unrestrict_primary_key
 end
--- lib/schema.rb
+++ lib/schema.rb
@@ -10,17 +10,41 @@ class RecordLoader < GraphQL::Batch::Loader
   end
 
   def perform(ids)
-    if @model == UnitItem
-      result = Hash.new { |h,k| h[k] = [] }
-      @model.where(item_id: ids, is_direct: true).order(:ordering_of_units).select(:item_id, :unit_id).each { |record|
-        result[record.item_id] << record
-      }
-      result.each { |k,v| fulfill(k, v) }
-    else
-      @model.where(id: ids).each { |record|
-        fulfill(record.id, record)
-      }
-    end
+    @model.where(id: ids).each { |record| fulfill(record.id, record) }
+    ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
+  end
+end
+
+###################################################################################################
+class CountLoader < GraphQL::Batch::Loader
+  def initialize(query, field)
+    @query = query
+    @field = field
+  end
+  def perform(ids)
+    @query.where(Hash[@field, ids]).group_and_count(@field).each { |row|
+      fulfill(row[@field], row[:count])
+    }
+    ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
+  end
+end
+
+###################################################################################################
+class GroupLoader < GraphQL::Batch::Loader
+  def initialize(query, field, limit = nil)
+    @query = query
+    @field = field
+    @limit = limit
+  end
+
+  def perform(ids)
+    result = Hash.new { |h,k| h[k] = [] }
+    @query.where(Hash[@field, ids]).each{ |record|
+      if !@limit || result[record[@field]].length < @limit
+        result[record[@field]] << record
+      end
+    }
+    result.each { |k,v| fulfill(k,v) }
     ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
   end
 end
@@ -113,10 +137,20 @@ ItemType = GraphQL::ObjectType.define do
     }
   end
 
+  field :authors, AuthorsType, "All authors (can be long)" do
+    argument :first, types.Int, default_value: 100, prepare: ->(val, ctx) {
+      (val.nil? || (val >= 1 && val <= 500)) or return GraphQL::ExecutionError.new("'first' must be in range 1..500")
+      return val
+    }
+    argument :more, types.String
+    resolve -> (obj, args, ctx) { AuthorsData.new(args, obj.id) }
+  end
+
   field :units, !types[UnitType], "The series/unit(s) associated with this item" do
     resolve -> (obj, args, ctx) {
-      RecordLoader.for(UnitItem).load(obj.id).then { |unitItems|
-        RecordLoader.for(Unit).load_many(unitItems.map { |unitItem| unitItem.unit_id })
+      query = UnitItem.where(is_direct: true).order(:item_id, :ordering_of_units).select(:item_id, :unit_id)
+      GroupLoader.for(query, :item_id).load(obj.id).then { |unitItems|
+        RecordLoader.for(Unit).load_many(unitItems ? unitItems.map { |unitItem| unitItem.unit_id } : [])
       }
     }
   end
@@ -243,6 +277,56 @@ class ItemsData
       more['lastDate'] = nodes[-1][field].iso8601
       @more = Base64.urlsafe_encode64(more.to_json)
     end
+  end
+end
+
+###################################################################################################
+AuthorsType = GraphQL::ObjectType.define do
+  name "Authors"
+  description "A list of authors, with paging capability because some items have thousands"
+  field :total, !types.Int, "Approximate total items on all pages"
+  field :nodes, !types[AuthorType], "Array of the items on this page"
+  field :more, types.String, "Opaque cursor string for next page"
+end
+
+###################################################################################################
+class AuthorsData
+  def initialize(args, itemID)
+    # If 'more' was specified, decode it and use all the parameters from the original query
+    @args = args['more'] ? JSON.parse(Base64.urlsafe_decode64(args['more'])) : args.to_h.clone
+
+    # Record the item ID for querying
+    @itemID = itemID
+  end
+
+  def total
+    CountLoader.for(ItemAuthor, :item_id).load(@itemID)
+  end
+
+  def nodes
+    query = ItemAuthor.order(:item_id, :ordering)
+    @args['lastOrd'] and query = query.where(Sequel.lit("ordering > ?", @args['lastOrd']))
+    @nodes ||= GroupLoader.for(query, :item_id, @args['first']).load(@itemID)
+  end
+
+  def more
+    nodes.then { |arr|
+      if arr && arr.length == @args['first']
+        Base64.urlsafe_encode64(@args.merge({lastOrd: arr[-1].ordering}).to_json)
+      else
+        nil
+      end
+    }
+  end
+end
+
+###################################################################################################
+AuthorType = GraphQL::ObjectType.define do
+  name "Author"
+  description "A single author (can be a person or organization)"
+
+  field :name, !types.String, "Combined name parts; usually 'lname, fname'" do
+    resolve -> (obj, args, ctx) { JSON.parse(obj.attrs)['name'] }
   end
 end
 
