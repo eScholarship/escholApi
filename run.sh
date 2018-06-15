b896b6adbb959e53442c92f6a7cc53e54ab4fc68
Martin Haye <martin.haye@ucop.edu>
2018-06-15 10:35:47 -0700

    Updated port to match real server environment.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-RACK_ENV=development bundle exec rerun -d lib --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p 3000
+RACK_ENV=development bundle exec rerun -d lib --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p 18900
