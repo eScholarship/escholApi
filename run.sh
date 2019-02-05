93376d67c05de3763dda439fa3e68ee8786ee5aa
Martin Haye <martin.haye@ucop.edu>
2019-02-04 17:02:31 -0800

    Little changes to work on dev or local.

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-[ -z $ESCHOL_DB_ADAPTER ] && source config/env.sh
+[ -z $ESCHOL_DB_ADAPTER ] && [ -f config/env.sh ] && source config/env.sh
+[ -z $ESCHOL_DB_ADAPTER ] && [ -f config/env-dev.sh ] && source config/env-dev.sh
 bundle install --quiet --with development --path=gems --binstubs
-RACK_ENV=development bundle exec rerun -d lib --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p 18900
+RACK_ENV=development bundle exec rerun -d lib --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p $PUMA_PORT
