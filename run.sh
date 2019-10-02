66b2240fc8be358ec13cc5c1d202bd3fce7db8e7
Martin Haye <martin.haye@ucop.edu>
2019-10-02 09:03:32 -0700

    Get rid of log messages due to rerun's foreground behavior.

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -e
 [ -z $ESCHOL_DB_ADAPTER ] && [ -f config/env.sh ] && source config/env.sh
 [ -z $ESCHOL_DB_ADAPTER ] && [ -f config/env-dev.sh ] && source config/env-dev.sh
 bundle install --quiet --with development --path=gems --binstubs
-RACK_ENV=development bundle exec rerun -d lib --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p $PUMA_PORT
+RACK_ENV=development bundle exec rerun -d lib --background --no-notify --pattern '**/*.rb' -- rackup -o 0.0.0.0 -p $PUMA_PORT
